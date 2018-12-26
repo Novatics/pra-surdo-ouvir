944c8013e4a29277e8df72dbf75bd975d3aa20b9
flavioalves <flaviof.alves@gmail.com>
2018-12-26 18:29:34 -0200

    Remove streams attribute obligation

--- index.js
+++ index.js
@@ -2,13 +2,37 @@ const { prisma } = require("./generated/prisma-client");
 
 // A `main` function so that we can use async/await
 async function main() {
-  // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: "Alice" });
+  // Create a new user with a new post
+  const newUser = await prisma.createUser({
+    name: "Bob",
+    email: "bob@prisma.io",
+    streams: {
+      create: [
+        {
+          title: "Join us for GraphQL Conf in 2019",
+          description: "Just a first description",
+          createdAt: new Date(),
+          category: "TECH",
+          language: "pt-br"
+        },
+        {
+          title: "Subscribe to GraphQL Weekly for GraphQL news",
+          description: "A second description",
+          createdAt: new Date(),
+          category: "TECH",
+          language: "pt-br"
+        }
+      ]
+    }
+  });
   console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
 
   // Read all users from the database and print them to the console
   const allUsers = await prisma.users();
   console.log(allUsers);
+
+  const allPosts = await prisma.posts();
+  console.log(allPosts);
 }
 
 main().catch(e => console.error(e));
