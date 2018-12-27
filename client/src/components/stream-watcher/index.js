a512f76a12c282a5edee3de9389ed1975ef431e3
Levino <levinomoises@gmail.com>
2018-12-27 17:33:17 -0200

    Add socketio to stream watcher

--- client/src/components/stream-watcher/index.js
+++ client/src/components/stream-watcher/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 import { Link } from "@reach/router"
+import io from 'socket.io-client';
 
 export default function StreamWatcher({ id }) {
+  const socket = io('http://localhost');
+
+  socket.on(id, function (from, msg) {
+    return (
+      <div> msg </div>
+    )
+  });
+
   return(
     <div>
       <Link to="/">Home</Link>{" "}
