7d6d3615c938f3e6adfcca9968e7669e6c0b0a46
Levino <levinomoises@gmail.com>
2018-12-28 12:14:15 -0200

    Update stream recorder to record audio

--- src/client/components/stream-recorder/index.js
+++ src/client/components/stream-recorder/index.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-
+import Button from 'antd/lib/button';
 import { Link } from '@reach/router';
+import AudioStreamer from '../../util/audioStreamer';
 
 class StreamRecorder extends Component {
+  state = {
+    streamText: '',
+  }
+
   componentWillUnmount() {
     // TODO - Here we should call to kill this stream
   }
 
+  startStream = () => {
+    AudioStreamer.initRecording((data) => {
+      this.setState({ streamText: data.results[0].alternatives[0].transcript });
+    }, (error) => {
+      console.log(error);
+    });
+  }
+
   render() {
+    const { streamText } = this.state;
+
     return (
       <div>
         <Link to="/">Home</Link>
         {' '}
-Create stream route
+        Create stream route
+        <Button onClick={this.startStream} type="primary">Record</Button>
+        <span>{streamText}</span>
       </div>
     );
   }
