46b3e0a076e6daf0d391fea629bd6aa7fa130971
flavioalves <flaviof.alves@gmail.com>
2018-12-28 10:16:09 -0200

    Change graphql api and andpoint routes

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,7 +1,7 @@
 const { GraphQLServer } = require('graphql-yoga');
 const os = require('os');
 const express = require('express');
-const prisma = require('./generated/prisma-client');
+const { prisma } = require('./generated/prisma-client');
 const resolvers = require('./resolvers');
 
 const server = new GraphQLServer({
@@ -14,9 +14,9 @@ const server = new GraphQLServer({
 
 server.express.use(express.static('dist'));
 server.express.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }),);
-server.express.get('/teste', (req, res, next) => {
-  console.log('Request Type:', req.method);
-  next();
-});
 
-server.start(() => console.log('🚀  Server is running on http://localhost:4000'));
+const options = {
+  endpoint: '/graphql',
+  playground: '/playground',
+};
+server.start(options, () => console.log('🚀  Server is running on http://localhost:4000'));
