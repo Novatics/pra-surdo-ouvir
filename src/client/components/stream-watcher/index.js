fdefebbfd8c261465ee65e2afb784371f6eca99f
Vinicius Franco da Cunha Arantes <viniciusf.arantes@gmail.com>
2018-12-28 19:11:19 -0200

    Transforms stream-watcher into class component and adds audioStreamer util

--- src/client/components/stream-watcher/index.js
+++ src/client/components/stream-watcher/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from '@reach/router';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import Socket from 'socket.io-client';
 
 const GET_STREAM = gql`
   query Stream($streamId: ID!) {
@@ -12,22 +13,42 @@ const GET_STREAM = gql`
     }
   }`;
 
-function StreamWatcher({ id }) {
-  return (
-    <Query query={GET_STREAM} variables={{ streamId: id }}>
-      {({ loading, error, data }) => {
-        if (loading) return 'Loading...';
-        if (error) return `Error! ${error.message}`;
+class StreamWatcher extends React.Component {
+  state = {
+    streamText: '',
+  };
 
-        return (
-          <div>
-            <Link to="/">Home</Link>
-            Stream {data.stream.title} comendo solta!
-          </div>
-        );
-      }}
-    </Query>
-  );
+  componentDidMount() {
+    const socket = Socket('172.16.140.120:5000');
+    const { id } = this.props;
+    console.log(`speechData-${id}`);
+    socket.on(`speechData-${id}`,
+      (data) => {
+        console.log(data);
+        this.setState({ streamText: data.results[0].alternatives[0].transcript });
+      });
+  }
+
+  render() {
+    const { id } = this.props;
+    const { streamText } = this.state;
+    return (
+      <Query query={GET_STREAM} variables={{ streamId: id }}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...';
+          if (error) return `Error! ${error.message}`;
+
+          return (
+            <div>
+              <Link to="/">Home</Link>
+              <h1>Stream {data.stream.title} comendo solta!</h1>
+              <p>{ streamText }</p>
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
 }
 
 StreamWatcher.propTypes = {
