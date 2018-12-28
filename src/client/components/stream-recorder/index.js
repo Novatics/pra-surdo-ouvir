9f3aed2a3cc7fc883cc9277b00705156b64b26b5
Vinicius Franco da Cunha Arantes <viniciusf.arantes@gmail.com>
2018-12-28 19:13:01 -0200

    Stream microphone recording implemented

--- src/client/components/stream-recorder/index.js
+++ src/client/components/stream-recorder/index.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 
 import { Link } from '@reach/router';
+import Button from 'antd/lib/button';
+import AudioStreamer from '../../util/audioStreamer';
 
 class StreamRecorder extends Component {
+  state = {
+    isRecording: false,
+  }
+
   componentWillUnmount() {
     // TODO - Here we should call to kill this stream
   }
 
+  startStream = () => {
+    const tmpID = 'cjq5xi8zy1tp00a55qyd1lic8';
+    this.setState({ isRecording: true });
+    AudioStreamer.initRecording(tmpID);
+  }
+
+  stopStrem = () => {
+    this.setState({ isRecording: false });
+    AudioStreamer.stopRecording();
+  }
+
   render() {
+    const { isRecording } = this.state;
     return (
       <div>
         <Link to="/">Home</Link>
-        {' '}
-Create stream route
+        Create stream route
+        <Button
+          onClick={isRecording ? this.stopStream : this.startStream}
+          type="primary"
+        >
+          { isRecording ? 'Stop' : 'Start' }
+        </Button>
+        <br />
       </div>
     );
   }
